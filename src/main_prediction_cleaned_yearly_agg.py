d791ddf2dd129d0d3b642ee96011289d7884f3bf
markmorr@usc.edu <markmorr@usc.edu>
2022-05-02 22:25:39 -0400

    cleaning main_prediction_monthly

--- src/main_prediction_cleaned_yearly_agg.py
+++ src/main_prediction_cleaned_yearly_agg.py
@@ -40,24 +40,22 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.metrics import accuracy_score
+
 from prophet import Prophet
+from catboost import CatBoostClassifier
+from catboost import CatBoostRegressor, Pool
 
 
 from temp_state_dict_working import getCodeToAreaDict, getAreaToCodeDict, monthToNumberDict
 from useful_code import correctDamageProp
 from state_to_neighbor_dict import get_state_border_dict
 from saving_files_locally import get_df_dict
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, confusion_matrix
-
 from us_state_to_abbrev import get_us_state_to_abbrev
 
-# TODOs  
-def getResults(y_true, y_pred):
-    print('MAE is: ' + str(mean_absolute_error(y_true,y_pred)))
+
 
 
 interimDataPath = r'C:\Users\16028\Downloads\storm_details'
@@ -79,7 +77,6 @@ plt.show()
 # df['season'] = df['month'].apply(mapToSeason)
 
         
-
 BASE_YEAR = 1980 #***
 df['year'] = df['begin_yearmonth'].astype(str).str[:4].astype(int)
 df['month'] = df['begin_yearmonth'].astype(str).str[4:].astype(int)
@@ -97,12 +94,8 @@ storm_exclusion_list = ['Drought', 'Heat', 'Excessive Heat', 'High Surf', 'Wildf
                         'Avalanche', 'Marine High Wind', 'Funnel Cloud', 'Rip Current',
                         'Volcanic Ash', 'Dense Smoke']
 
-
-df2 = df.copy()
-
 df = df[~df['event_type'].isin(storm_exclusion_list)]
-
-df['num_storms'] = 1
+df['num_storms'] = 1 #used later for aggregating and counting the number of storms per state
 df.columns
 
 state_vc = df.state.value_counts()
@@ -194,74 +187,12 @@ df['day'] = 1
 df['month'] = 1
 df [['month','day']] = df [['month','day']].astype(str).apply(lambda x: x.str.zfill(2)) #*** deleted month
 
-
-
-
-# df['y'] = df['y'].clip(upper=15) #*** MIGHT NEED TO INCLUDE SINCE THIS IS AGGREGATE
-
-
 df_backup = df.copy()
 
-
-
-col_names = ['ghcn_id', 'lat', 'lon', 'stdv', 'yr_mo', 'network']
-
-df = pd.read_csv('https://www1.ncdc.noaa.gov/pub/data/cirs/climdiv/climdiv-ak-prcp-inv-v1.0.0-20220406',
-                  delim_whitespace=True, names= col_names)
-
-
-
-
-col_names = ['id', 'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'july', 'aug', 'sep',
-             'oct', 'nov', 'dec']
-
-df_dict = {}
-address_list_dict = {'prec': 'https://www1.ncdc.noaa.gov/pub/data/cirs/climdiv/climdiv-pcpnst-v1.0.0-20220406 ',
-                'tmax': 'https://www1.ncdc.noaa.gov/pub/data/cirs/climdiv/climdiv-tmaxst-v1.0.0-20220406',
-                'tmin':'https://www1.ncdc.noaa.gov/pub/data/cirs/climdiv/climdiv-tminst-v1.0.0-20220406',
-                'avg':'https://www1.ncdc.noaa.gov/pub/data/cirs/climdiv/climdiv-tmpcst-v1.0.0-20220406',
-                }
-
-# =============================================================================
-# for name, address in address_list_dict.items():
-#     df_dict[name] = pd.read_csv(address,
-#                       delim_whitespace=True, names= col_names, converters={'id': lambda x: str(x)})
-#     
-#     
-# df_name_list = ['prec', 'tmax', 'tmin', 'avg']
-# =============================================================================
-
 ####################################################################################
 ####################################################################################
 ####################################################################################
 
-
-
-
-# =============================================================================
-# col_names = ['id', 'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'july', 'aug', 'sep',
-#              'oct', 'nov', 'dec']
-# 
-# df_dict = {}
-# address_list_dict = {'prec': 'https://www1.ncdc.noaa.gov/pub/data/cirs/climdiv/climdiv-pcpnst-v1.0.0-20220406 ',
-#                 'tmax': 'https://www1.ncdc.noaa.gov/pub/data/cirs/climdiv/climdiv-tmaxst-v1.0.0-20220406',
-#                 'tmin':'https://www1.ncdc.noaa.gov/pub/data/cirs/climdiv/climdiv-tminst-v1.0.0-20220406',
-#                 'avg':'https://www1.ncdc.noaa.gov/pub/data/cirs/climdiv/climdiv-tmpcst-v1.0.0-20220406',
-#                 }
-# 
-# 
-# for name, address in address_list_dict.items():
-#     df_dict[name] = pd.read_csv(address,
-#                       delim_whitespace=True, names= col_names, converters={'id': lambda x: str(x)})
-#     
-#     
-# =============================================================================
-
-####################################################################################
-####################################################################################
-####################################################################################
-
-# TODO
 df_dict = get_df_dict()
 
 
@@ -292,17 +223,7 @@ for var_name in var_name_list:
     code_to_area_dict = getCodeToAreaDict()
     area_to_code_dict = getAreaToCodeDict()
     df['state'] = df['state_code'].map(code_to_area_dict)
-
-    # df_temp = df[names_of_month]
-    
-    # df_temp2 = df_temp.melt(id_vars='id')
-    
-    # month_to_number = monthToNumberDict()
-    # df_temp2['month'] = df_temp2['variable'].map(month_to_number)
-    # df = df_temp2.copy()
-    # df.rename(columns={'value': var_name}, inplace=True)
     df_processed_dict[var_name] = df[['id', var_name]].copy()
-    
     ##################################################
     
 
@@ -329,9 +250,6 @@ df_sum = sum_df.copy()
 df_new = pd.merge(df, sum_df, on =['year', 'state'], how='inner')
 df_new.to_csv(r'C:\Users\16028\Downloads\storm_figuring_out_stuff\df_new_yearly.csv', index=False)
 
-
-
-
 # closed = left ensures we don't use this month's precipitation average for our value
 #3 is the past 3 indices, it relies on perfect regularity, which seems correct at least
 # for these states and for this precipitation data
@@ -339,9 +257,6 @@ df_new.to_csv(r'C:\Users\16028\Downloads\storm_figuring_out_stuff\df_new_yearly.
 
 ########################################################################################
 #######################################################################################
-
-
-# window_lengths = [1,3,6,12,24]
 window_lengths = [1,2,3,5]
 feature_list = ['prec', 'tmpc']
 feature_name_list = []
@@ -373,16 +288,13 @@ def getRollingFeatures(df_input, feature_tuple_list, feature_name_list):
     df_input.reset_index(inplace=True)
     base_date = df_input.date.min()
     return df_input
-
-
-
-
-
 df_to_use = pd.read_csv(r'C:\Users\16028\Downloads\storm_figuring_out_stuff\df_new_yearly.csv')
 getRollingFeatures(df_to_use, feature_tuple_list, feature_name_list)
 
 
 #####################################################################################
+#####################################################################################
+
 
 df = pd.read_csv(r'C:\Users\16028\Downloads\storm_figuring_out_stuff\df_new_yearly.csv')
 df['date'] = pd.to_datetime(df[['year']].assign(DAY=1,  MONTH=1)).dt.date # deleted month, added it to assign
@@ -406,20 +318,6 @@ for k,v in state_to_neighbor_dict.items():
             v.remove(state_name)
             
 df['state_neighbors'] = df['state'].map(state_to_neighbor_dict)
-
-
-# df
-# rolling_feature_dict = dict()
-# k_list = [1,3,6,12,24]
-# feature_category = 'prec'
-# for state in state_list:
-#     for k in k_list:
-#         rolling_feature_dict['prec_' + state + '_' + str(k)] = df[df['state'] == state]['value'].transform(lambda x: x.rolling(window=k,  closed='left').mean())
-
-feature_name_list
-
-feature_tuple_list
-feature_name_list
 rolling_feature_dict = dict()
 for state in state_list:
     for feat_tuple, feat_name in zip(feature_tuple_list, feature_name_list):
@@ -448,12 +346,7 @@ def getNeighboringFeatures(row, k):
     else:
         return contribution/n_neighbors
     
-feature_name_list
-mystr = 'dogl'
-mystr.split('o')
 
-
-    
 added_feature_list = []
 for feat_name, feat_tuple, in zip(feature_name_list, feature_tuple_list):
     value = feat_tuple[0]
@@ -465,16 +358,12 @@ for feat_name, feat_tuple, in zip(feature_name_list, feature_tuple_list):
     df[feature_name] = df.apply(getNeighboringFeatures, args =(k,), axis=1)
     df[feature_name] = df[feature_name].fillna(method='bfill')
 
-feature_name
+
 
 df_with_neighbors = df.copy()
-# df_new = pd.merge(df_to_use, df, on =['year', 'month', 'state'], how='inner')
-
 ###############################################################################
 ###############################################################################
 ###############################################################################
-
-
 df = pd.read_csv(r'C:\Users\16028\Downloads\storm_figuring_out_stuff\df_new_yearly.csv') #*** changed to yearly
 df['date'] = pd.to_datetime(df[['year',]].assign(DAY=1, MONTH=1)).dt.date
 df.set_index('date', inplace=True, drop=True)
@@ -500,9 +389,7 @@ df['years_since'] = df['year'] - df.year.min()
 # df['state_encoded'] = df['state'].astype('category').cat.codes
 # =============================================================================
 # =============================================================================
-###############################################################################
-# TARGET ENCODING
-# ALP'S SUGGESTION OF ENCODING BY TARGET ORDERING--HELPED A LOT, .05-.06 increase in R2
+# TARGET ENCODING (ALP'S SUGGESTION OF ENCODING BY TARGET ORDERING--HELPED A LOT, .05-.06 increase in R2)
 df_state_group = df.groupby('state')
 te_dict = dict()
 for name, group in df_state_group:
@@ -516,11 +403,6 @@ df['state_encoded'] = df['state'].map(te_dict_ranking)
 key_columns = ['year', 'state']
 # df = pd.merge(df, df_with_neighbors[['year', 'month', 'state'] + added_feature_list], on=key_columns, how='inner')
 df = pd.merge(df, df_with_neighbors[['year', 'state'] + added_feature_list], on=key_columns, how='inner')
-
-added_feature_list
-df.columns
-df_with_neighbors.columns
-
 
 ####################################################################################
 def binarizeTarget(x):
@@ -538,7 +420,6 @@ df.drop(columns='log_cost')
 X = df.drop(columns=['log_cost', 'num_storms']).copy() #'month',
 y = df[['log_cost']].copy()
 
-
 df_backup_indices = df.copy()
 df.sort_values(by=['years_since'], inplace=True)
 
@@ -551,31 +432,13 @@ y_train = df[df['years_since'] <= split_year]['log_cost'].copy()
 y_test = df[df['years_since'] > split_year]['log_cost'].copy()
 
 
-
-
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=9)
 # X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size =.50, random_state=9)
 
 
 # print(y_train[y_train > 2000].shape[0]/y_train.shape[0])
 # print(y_test[y_test > 2000].shape[0]/y_test.shape[0])
-
-
-
-# # TODO
-# plt.hist(y_train, bins=20)
-
-# plt.scatter(np.sort(y))
-# plt.xlabel('X')
-
-# y_train
-# plt.hist(y_train,  cumulative=True, label='CDF',
-#          histtype='step', alpha=0.8, color='k')
-
-
 # df['cost_threshold'] = df['log_cost']
-
-# np.exp(900)
 
 
 def getMetrics(y_true, y_pred, flag, formulation= 'regression' ):
@@ -631,9 +494,6 @@ def callModel(clf,x_train,  y_train, x_test, y_test):
     # print('difference is: ' + str(accuracy_score(y_train, clf.predict(x_train)) - accuracy_score(y_test, clf.predict(x_test))))
     return
 
-
-from catboost import CatBoostClassifier
-from catboost import CatBoostRegressor, Pool
 model = CatBoostRegressor(random_seed=9,
                           loss_function='RMSE',
                           logging_level='Silent')
@@ -646,32 +506,14 @@ for reg in reg_list:
     runModel(reg, X_train, y_train, X_test, y_test) 
     
 ###################################################################################
-clf_list = [LogisticRegression(), RandomForestClassifier(max_depth=35), ExtraTreesClassifier(max_depth=30, min_samples_split=3),
-            GradientBoostingClassifier(),
-            CatBoostClassifier(random_seed=9,logging_level='Silent')]
-for clf in clf_list:
-    callModel(clf, X_train, y_train, X_test, y_test )
+# clf_list = [LogisticRegression(), RandomForestClassifier(max_depth=35), ExtraTreesClassifier(max_depth=30, min_samples_split=3),
+#             GradientBoostingClassifier(),
+#             CatBoostClassifier(random_seed=9,logging_level='Silent')]
+# for clf in clf_list:
+#     callModel(clf, X_train, y_train, X_test, y_test )
     
-y_test
-reg = RandomForestRegressor(max_depth=35)
-reg.fit(X_train, y_train)
-y_pred = reg.predict(X_train)
-# y_pred_e = np.exp(y_pred)
 
 
-y_pred
-# build up the code
-# # then move on to climate forward 
-# then expand the feature set
-# # binning/discretizing response storm amounts
-# # then go lower level to zip code
-# # # then connect it to housing prices
-# might need to do some work to ensure the neighboring variables always match with
-# the state's own features, but that may not be the case--> maybe just rearrange code
-
-# fix the index reset issue
-# pass in feature list as a parameter
-# get df.apply (args = (,)) to work
 
 
 
